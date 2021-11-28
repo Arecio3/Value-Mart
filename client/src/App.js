6dab329620b2fb1916e99363939a15b63097b381
Arecio Canton <devmain@Arecios-iMac.local>
2021-11-28 15:18:55 -0500

    Added Ant-design working on Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/nav/Header';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
 
 const App = () => {
   return (
+    <>
+      <Header />
     <Router>
    <Routes>
      <Route exact path='/' element={<Home />}/>
@@ -12,6 +15,7 @@ const App = () => {
      <Route exact path='/register' element={<Register/>}/>
    </Routes>
     </Router>
+    </>
   );
 }
 
