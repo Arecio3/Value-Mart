c242c7dd96b35b9562f37c5d5b45c43bfb4e9980
Arecio Canton <devmain@Arecios-iMac.local>
2021-12-01 00:30:19 -0500

    We got token

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
 import Header from './components/nav/Header';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
 import RegisterFinal from './pages/auth/RegisterFinal';
+
+import { auth } from './firebase'; 
+import { useDispatch } from 'react-redux';
+
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  // Check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if(user) {
+        const idTokenResult = await user.getIdTokenResult()
+        
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token
+          }
+        })
+      }
+    })
+    // Clean up
+    return () => unsubscribe();
+  }, [])
+
+
   return (
     <>
       <Header />
