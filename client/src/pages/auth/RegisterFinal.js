023cecafbff95d1100f889ce8eb3fcdcc611c985
Arecio Canton <devmain@Arecios-iMac.local>
2021-11-30 23:51:00 -0500

    Successfully logging user into db

--- client/src/pages/auth/RegisterFinal.js
+++ client/src/pages/auth/RegisterFinal.js
@@ -1,23 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import bg1 from '../../assets/register-pic1.svg';
-import bg2 from '../../assets/register-pic2.svg';
+import bg1 from '../../assets/registerfinal1.png';
+import bg2 from '../../assets/registerfinal2.png';
 import '../../styles/register.css';
 import { auth } from '../../firebase';
 import { toast } from 'react-toastify';
+import '../../styles/registerfinal.css';
 require("firebase/auth");
 
 toast.configure()
 const RegisterFinal = ({history}) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+    // Grabs email from local Storage and sets it to the email state
     useEffect(() => {
         // Grab email from local storage
-        setEmail(window.localStorage.getItem('emailForRegistration'))
-    }, [])
+        setEmail(window.localStorage.getItem('emailForRegistration'));
+    }, []);
     
     const handleSubmit = async (e) => {
         e.preventDefault();
+        // Sign in
+        try {
+            const result = await auth.signInWithEmailLink(email, window.location.href);
+            console.log('User Successfully created', result)
+
+            if(result.user.emailVerified) {
+                // Delete email from local storage
+                window.localStorage.removeItem('emailForRegistration')
+                // get current user logged into firebase
+                let user = auth.currentUser
+                // Update password
+                await user.updatePassword(password)
+                // Get id
+                const idTokenResult = await user.getIdTokenResult();
+                // populate user to redux store
+                console.log(idTokenResult)
+                // redirect user back
+                history.push('/')
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
     }
 
     // Form UI
@@ -30,7 +54,16 @@ const RegisterFinal = ({history}) => {
              value={email} 
              autoFocus
              />
-             <button type="submit" className="btn btn-green m-3">Complete Registration</button>
+             <br />
+            <input
+             placeholder='Password' 
+             type="password" 
+             className="form-control" 
+             value={password}
+             onChange={(e) => setPassword(e.target.value)}
+             autoFocus 
+             />
+             <button type="submit" className="btn btn-green m-3">Start Shopping</button>
         </form>
     
 
@@ -38,16 +71,16 @@ const RegisterFinal = ({history}) => {
         <div className="container p-5">
             <div className="row">
                 <div className="col-md-6 offset-md-3">
-                    <h2 className='heading'>Register Now</h2>
-                    <h6 className='subhead'>Start shopping today !</h6>
+                    <h2 className='heading'>Final Step</h2>
+                    <h6 className='subhead'>Once submitted you will be returned to the shop</h6>
                     {completeRegistrationForm()}
                 </div>
             </div>
-            <div className="img1-box img-fluid">
-                <img className="img1" src={bg1} alt="" />
+            <div className="registerImg1-box img-fluid">
+                <img className="registerImg1" src={bg1} alt="" />
             </div>      
-            <div className="img2-box img-fluid">
-                <img  className="img2" src={bg2} alt="" />
+            <div className="registerImg2-box img-fluid">
+                <img  className="registerImg2" src={bg2} alt="" />
             </div>      
         </div>
     )
