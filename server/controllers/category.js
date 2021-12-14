7e0ae60d69f12b5ef61519d4ad51ff3868e96acc
Arecio Canton <areciocodes@gmail.com>
2021-12-14 07:12:05 -0500

    You can get one category and delete

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -11,16 +11,24 @@ exports.createCat = async (req, res) => {
         res.status(400).send('Category was NOT created')
     }
 }
+// Get list of all categories
+exports.listCat = async (req, res) =>  res.json(await Category.find({}).sort({createdAt: -1}).exec())
 
-exports.listCat = async (req, res) => {
-
-}
+// Get one Category
 exports.readCat = async (req, res) => {
-
+let category = await Category.findOne({slug: req.params.slug}).exec();
+res.json(category)
 }
 exports.updateCat = async (req, res) => {
 
 }
+// Delete
 exports.deleteCat = async (req, res) => {
+try {
+    const deleted = await Category.findOneAndDelete({slug: req.params.slug})
+    res.json(deleted)
 
+} catch (err) {
+    res.status(400).send("Category was not deleted")
+}
 }
